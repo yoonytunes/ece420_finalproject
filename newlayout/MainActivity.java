3954c0110b8d7f5f55788132bfe46990de866a7d
eliasco2 <eliasco2@illinois.edu>
2023-04-24 17:43:40 -0500

    new layout with slider

---
+++ newlayout/MainActivity.java
@@ -0,0 +1,72 @@
+package com.ece420.lab6;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ActivityInfo;
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Flag to control app behavior
+    public static int qf = 50;
+    // UI Variables
+    private Button histeqButton;
+    private SeekBar seekBar;
+    private TextView sliderText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        super.setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Request User Permission on Camera
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);}
+
+        // Setup Button for Histogram Equalization
+        histeqButton = (Button) findViewById(R.id.histeqButton);
+        histeqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                qf = qf;
+                startActivity(new Intent(MainActivity.this, ShowActivity.class));
+            }
+        });
+
+        // Setup SeekBar
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setProgress(50);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                qf = progress;
+                sliderText.setText(String.valueOf(qf));
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        // Setup Slider Text
+        sliderText = (TextView) findViewById(R.id.sliderView);
+        sliderText.setText("50");
+    }
+
+    @Override
+    protected void onResume(){
+        super.setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onResume();
+    }
+
+}
---
+++ newlayout/ShowActivity.java
@@ -0,0 +1,125 @@
+package com.ece420.lab6;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+// import java.util.List;
+
+
+public class ShowActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    // UI Variable
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private SurfaceView surfaceView2;
+    private SurfaceHolder surfaceHolder2;
+    private TextView qfText;
+    private TextView ratioText;
+
+    // Camera Variable
+    private Camera camera;
+    boolean previewing = false;
+    private int width = 640;
+    private int height = 480;
+    private int qf = MainActivity.qf;
+
+    private Canvas canvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_show);
+        super.setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Setup Surface View handler
+        surfaceView = (SurfaceView)findViewById(R.id.ViewOrigin);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView2 = (SurfaceView)findViewById(R.id.ViewHisteq);
+        surfaceHolder2 = surfaceView2.getHolder();
+
+        // Setup Text Views
+        qfText = (TextView) findViewById(R.id.qfText);
+        qfText.setText(String.valueOf(qf));
+
+        ratioText = (TextView) findViewById(R.id.ratioText);
+        ratioText.setText("N/A");
+    }
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Must have to override native method
+        return;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if(!previewing) {
+            camera = Camera.open();
+            if (camera != null) {
+                try {
+                    // Modify Camera Settings
+                    Camera.Parameters parameters = camera.getParameters();
+                    parameters.setPreviewSize(width, height);
+                    // Following lines could log possible camera resolutions, including
+                    // 2592x1944;1920x1080;1440x1080;1280x720;640x480;352x288;320x240;176x144;
+                    // List<Camera.Size> sizes = parameters.getSupportedPictureSizes();
+                    // for(int i=0; i<sizes.size(); i++) {
+                    //     int height = sizes.get(i).height;
+                    //     int width = sizes.get(i).width;
+                    //     Log.d("size: ", Integer.toString(width) + ";" + Integer.toString(height));
+                    // }
+                    camera.setParameters(parameters);
+                    camera.setDisplayOrientation(90);
+                    camera.setPreviewDisplay(surfaceHolder);
+                    camera.setPreviewCallback(new PreviewCallback() {
+                        public void onPreviewFrame(byte[] data, Camera camera)
+                        {
+                            // Lock canvas
+                            //canvas = surfaceHolder2.lockCanvas(null);
+                            // Where Callback Happens, camera preview frame ready
+                            //onCameraFrame(canvas,data);
+                            // Unlock canvas
+                            //surfaceHolder2.unlockCanvasAndPost(canvas);
+                        }
+                    });
+                    camera.startPreview();
+                    previewing = true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Cleaning Up
+        if (camera != null) {
+            camera.stopPreview();
+            camera.setPreviewCallback(null);
+            camera.release();
+            camera = null;
+            previewing = false;
+        }
+    }
+}
